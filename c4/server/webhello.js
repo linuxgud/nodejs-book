173c2c4ad376a320375807513fc47d0c1bf108d0
Michal Kment <michal.kment@ki.se>
2023-03-08 16:42:20 +0100

    first 4 capters done

---
+++ c4/server/webhello.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const port = process.argv[2] || process.env.PORT || 3000,
+  http = require("http");
+
+http
+  .createServer((req, res) => {
+    console.log(req.url);
+
+    // abort favicon.ico request
+    if (req.url.includes("favicon.ico")) {
+      res.statusCode = 404;
+      res.end("Not found");
+      return;
+    }
+
+    const nameArg = capitalize(
+      req.url
+        .replace(/[^\w.,-]/g, " ")
+        .replace(/\s+/g, " ")
+        .trim() || "world"
+    );
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end(`<p>Hello ${nameArg}!</p>`);
+  })
+  .listen(port);
+
+console.log(`Server running at http://localhost:${port}/`);
+
+function capitalize(str) {
+  return str
+    .trim()
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
